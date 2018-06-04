78e14e7448caa63c12a4c6b450a0e30b4233cb54
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-04 17:51:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli=[]
+var newName
+var katzDeliLine=[]
+
+function takeANumber(katzDeliLine,newName){
+  
+    katzDeliLine.push(newName)
+    return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  var i=0
+  while (i<katzDeliLine.length){
+  if(katzDeliLine.length>0){
+  return `Currently serving ${katzDeliLine[i]}.`
+  katzDeliLine.shift()
+  i++
+}
+else{
+  return "There is nobody waiting to be served!"
+}
+}
+
+}
+function currentLine(katzDeliLine){
+var i
+if (katzDeliLine.length>0){
+  for (i=0;i<katzDeliLine.length;i++){
+    return `"The line is currently: ${i+1}. ${katzDeliLine[i]}"`
+  }
+}
+  else {
+    return "The line is currently empty."
+  }
+}
